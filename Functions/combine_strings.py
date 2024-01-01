b6a9429b76ba390378b23923c33d5e65e5918aee
Slavi Dimitrov <135168991+sldimitrov@users.noreply.github.com>
2024-01-01 16:57:31 +0200

    Update combine_strings.py
    Adding comments to a file

--- Functions/combine_strings.py
+++ Functions/combine_strings.py
@@ -1,3 +1,5 @@
+# Define a function
 def combine_names(first_name: str, last_name: str):
+    #formatting the names into a bigger string
     fullname = f"{first_name} {last_name}"
-    return fullname
+    return fullname 
